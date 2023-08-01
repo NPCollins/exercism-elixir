6a13ac1ef326ac709b0d0095d950878538bfc8a4
Nicholas Collins <npcollins560@gmail.com>
2023-07-31 21:17:16 -0400

    Completed freelancer rates, all tests passing

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -4,14 +4,18 @@ defmodule FreelancerRates do
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount * (1.0 - (discount / 100.0))
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    daily_rate(hourly_rate) * 22
+    |> apply_discount(discount)
+    |> Float.ceil()
+    |> trunc
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    budget / (apply_discount(hourly_rate, discount) * 8.0)
+    |> Float.floor(1)
   end
 end
