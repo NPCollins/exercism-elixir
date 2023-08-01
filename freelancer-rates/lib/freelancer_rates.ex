d8ba129775398b21a9cf435021693e2c9c324000
Nicholas Collins <npcollins560@gmail.com>
2023-07-31 21:06:07 -0400

    PUlled new exercised

---
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -0,0 +1,17 @@
+defmodule FreelancerRates do
+  def daily_rate(hourly_rate) do
+    hourly_rate * 8.0
+  end
+
+  def apply_discount(before_discount, discount) do
+    # Please implement the apply_discount/2 function
+  end
+
+  def monthly_rate(hourly_rate, discount) do
+    # Please implement the monthly_rate/2 function
+  end
+
+  def days_in_budget(budget, hourly_rate, discount) do
+    # Please implement the days_in_budget/3 function
+  end
+end
