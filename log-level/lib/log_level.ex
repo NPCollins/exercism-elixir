8250ce26cbbdcdf410a033b7f61e1fe7e7f5d24a
Nicholas Collins <npcollins560@gmail.com>
2023-08-06 14:54:04 -0400

    Completed log level exercise

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,9 +1,24 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    cond do
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      legacy? == true -> :unknown
+      level == 0 -> :trace
+      level == 5 -> :fatal
+      true -> :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    label = to_label(level, legacy?)
+    cond do
+      label == :error or label == :fatal -> :ops
+      legacy? == true and label == :unknown -> :dev1
+      label == :unknown -> :dev2
+      true -> :false
+    end
   end
 end
