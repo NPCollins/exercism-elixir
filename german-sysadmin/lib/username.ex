388fbf401de83444dd571d8a68ba709ac3e7ccdb
Nicholas Collins <npcollins560@gmail.com>
2023-08-21 05:04:07 -0400

    Completed German sysadmin

--- german-sysadmin/lib/username.ex
+++ german-sysadmin/lib/username.ex
@@ -1,10 +1,16 @@
 defmodule Username do
-  def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
-
-    # Please implement the sanitize/1 function
+  @spec sanitize(charlist(), charlist()) :: charlist()
+  def sanitize(email, out \\ [])
+  def sanitize([], out), do: out
+  def sanitize([head | tail], out) do
+    case head do
+      head when head >= ?a and head <= ?z -> sanitize(tail, out ++ [head])
+      head when head == ?_ -> sanitize(tail, out ++ [head])
+      head when head == ?ä -> sanitize(tail, out ++ [?a, ?e])
+      head when head == ?ö -> sanitize(tail, out ++ [?o, ?e])
+      head when head == ?ü -> sanitize(tail, out ++ [?u, ?e])
+      head when head == ?ß -> sanitize(tail, out ++ [?s, ?s])
+      _ -> sanitize(tail, out)
+    end
   end
 end
