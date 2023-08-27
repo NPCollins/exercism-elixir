a67971ad26841d0d8d382d9abab1bb23a13c87d3
Nicholas Collins <npcollins560@gmail.com>
2023-08-27 14:32:37 -0400

    Completed DNA

--- dna-encoding/lib/dna.ex
+++ dna-encoding/lib/dna.ex
@@ -1,17 +1,41 @@
 defmodule DNA do
+  @spec encode_nucleotide(charlist()) :: non_neg_integer()
   def encode_nucleotide(code_point) do
-    # Please implement the encode_nucleotide/1 function
+    case code_point do
+      ?\s -> 0b0000
+      ?A -> 0b0001
+      ?C -> 0b0010
+      ?G -> 0b0100
+      ?T -> 0b1000
+    end
   end
 
+  @spec decode_nucleotide(non_neg_integer()) :: charlist()
   def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
+    case encoded_code do
+       0b0000 -> ?\s
+       0b0001 -> ?A 
+       0b0010 -> ?C 
+       0b0100 -> ?G 
+       0b1000 -> ?T     
+    end
   end
 
+  @spec encode(charlist()) :: bitstring()
   def encode(dna) do
-    # Please implement the encode/1 function
+    do_encode(dna, <<>>)
+  end
+  defp do_encode([], acc), do: acc
+  defp do_encode([nucleotide | rest], acc) do 
+    do_encode(rest, <<acc::bitstring, encode_nucleotide(nucleotide)::size(4)>>)
   end
 
+  @spec decode(bitstring()) :: charlist()
   def decode(dna) do
-    # Please implement the decode/1 function
+    do_decode(dna, [])
+  end
+  defp do_decode(<<>>, acc), do: Enum.reverse(acc)
+  defp do_decode(<<nucleotide::4, rest::bitstring>>, acc) do
+    do_decode(rest, [decode_nucleotide(nucleotide) | acc])
   end
 end
