9e22863a545fa6f048bd4ae6fa6f753436e200c1
Nicholas Collins <npcollins560@gmail.com>
2023-08-30 00:40:48 -0400

    Completed boutique inventory

--- boutique-inventory/lib/boutique_inventory.ex
+++ boutique-inventory/lib/boutique_inventory.ex
@@ -1,21 +1,28 @@
 defmodule BoutiqueInventory do
   def sort_by_price(inventory) do
-    # Please implement the sort_by_price/1 function
+    Enum.sort_by(inventory, &(&1.price), :asc)
   end
 
   def with_missing_price(inventory) do
-    # Please implement the with_missing_price/1 function
+    Enum.reject(inventory, &(&1.price))
   end
 
   def update_names(inventory, old_word, new_word) do
-    # Please implement the update_names/3 function
+    Enum.map(inventory, fn item -> 
+      Map.replace(item, :name, String.replace(item.name, old_word, new_word))
+    end)
   end
 
   def increase_quantity(item, count) do
-    # Please implement the increase_quantity/2 function
+    new_quantities = item[:quantity_by_size] 
+      |> Map.to_list 
+      |> Map.new(fn {key, size} -> {key, size + count} end)
+    Map.replace(item, :quantity_by_size, new_quantities)
   end
 
   def total_quantity(item) do
-    # Please implement the total_quantity/1 function
+    item[:quantity_by_size] 
+      |> Map.to_list 
+      |> Enum.reduce(0, fn {_, quantity}, acc -> acc + quantity end) 
   end
 end
