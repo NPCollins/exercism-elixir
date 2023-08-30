c2594c099b65631c465f6a7ac8ece689911a802a
Nicholas Collins <npcollins560@gmail.com>
2023-08-29 23:39:40 -0400

    Completed basketball website

--- basketball-website/lib/basketball_website.ex
+++ basketball-website/lib/basketball_website.ex
@@ -1,9 +1,14 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+     do_extract_from_path(data, String.split(path, "."))
+  end
+
+  defp do_extract_from_path(data, []), do: data
+  defp do_extract_from_path(data, [head | tail]) do
+    do_extract_from_path(data[head], tail)
   end
 
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    get_in(data, String.split(path, "."))
   end
 end
