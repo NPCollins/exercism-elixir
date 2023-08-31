475f2fda0c96fbc3ff4a0b9bb1a4359f1878e09c
Nicholas Collins <npcollins560@gmail.com>
2023-08-30 20:34:51 -0400

    Completed file sniffer

--- file-sniffer/lib/file_sniffer.ex
+++ file-sniffer/lib/file_sniffer.ex
@@ -1,13 +1,36 @@
 defmodule FileSniffer do
+  @spec type_from_extension(String.t()) :: String.t() | nil
   def type_from_extension(extension) do
-    # Please implement the type_from_extension/1 function
+    case extension do
+      "exe" -> "application/octet-stream"
+      "bmp" -> "image/bmp"
+      "png" -> "image/png"
+      "jpg" -> "image/jpg"
+      "gif" -> "image/gif"
+      _ -> nil
+    end
   end
 
-  def type_from_binary(file_binary) do
-    # Please implement the type_from_binary/1 function
+  @spec type_from_binary(binary()) :: String.t() | nil
+  def type_from_binary(file) do
+    case file do
+      <<0x7F, 0x45, 0x4C, 0x46, _::binary>> -> "application/octet-stream"
+      <<0x42, 0x4D, _::binary>> -> "image/bmp"
+      <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>> -> "image/png"
+      <<0xFF, 0xD8, 0xFF, _::binary>> -> "image/jpg"
+      <<0x47, 0x49, 0x46, _::binary>> -> "image/gif"
+      _ -> nil
+    end
   end
 
+  @spec verify(binary(), String.t()) :: {atom(), String.t()}
   def verify(file_binary, extension) do
-    # Please implement the verify/2 function
+    extension_type = type_from_extension(extension)
+    binary_type = type_from_binary(file_binary)
+    if (extension_type == binary_type) and extension_type != nil do
+      {:ok, extension_type}
+    else
+      {:error, "Warning, file format and file extension do not match."}
+    end
   end
 end
